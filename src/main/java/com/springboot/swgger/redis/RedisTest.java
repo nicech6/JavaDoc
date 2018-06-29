35f96150879f58f2b6f3a422059a06cd669ab719
崔海 <cuihai@wisezone.org>
2018-06-29 18:07:09 +0800

    java 连接redis

--- src/main/java/com/springboot/swgger/redis/RedisTest.java
+++ src/main/java/com/springboot/swgger/redis/RedisTest.java
@@ -1,8 +1,29 @@
 package com.springboot.swgger.redis;
 
+import com.springboot.swgger.UserInfo;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.cache.CacheProperties;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.data.redis.connection.jedis.JedisConnection;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import redis.clients.jedis.Jedis;
+
+import javax.annotation.Resource;
 
 public class RedisTest {
+    private Jedis jedis;
+
+    @Test
+    public void test(){
+        //连接redis服务器(在这里是连接本地的)
+        jedis = new Jedis("127.0.0.1", 6379);
+        //权限认证
+//        jedis.auth("cuihai");
+        System.out.println("连接服务成功");
+
+        System.out.println(""+jedis.get("name"));
+    }
 }
